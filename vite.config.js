2889b53b852c346bbbe9bb2ba5c1a1c05daa24f4
samadabdul768 <abdulabsolutetravel000@gmail.com>
2026-03-13 23:18:02 +0530

    Fix Vite config for Tailwind and React build

--- vite.config.js
+++ vite.config.js
@@ -8,3 +8,10 @@ export default defineConfig({
     tailwindcss(),
   ],
 })
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
